8e540b7ead0d6985052247e84be30a0fee2a13a9
Bernd Schoolmann <mail@quexten.com>
2025-06-02 12:27:59 +0200

    Move testable userkey rotation service code to testable class

--- apps/web/src/app/key-management/key-rotation/user-key-rotation.module.ts
+++ apps/web/src/app/key-management/key-rotation/user-key-rotation.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from "@angular/core";
 
+import { TestableUserKeyRotationService } from "./testable-user-key-rotation.service";
 import { UserKeyRotationApiService } from "./user-key-rotation-api.service";
-import { UserKeyRotationService } from "./user-key-rotation.service";
 
 @NgModule({
-  providers: [UserKeyRotationService, UserKeyRotationApiService],
+  providers: [TestableUserKeyRotationService, UserKeyRotationApiService],
 })
 export class UserKeyRotationModule {}
