e486a151d426b92fddbf919cf728c97f940ec974
jng <jng@bitwarden.com>
2025-06-20 14:01:56 -0400

    update for browser show more/less toggle

--- libs/vault/src/cipher-view/item-details/item-details-v2.component.ts
+++ libs/vault/src/cipher-view/item-details/item-details-v2.component.ts
@@ -1,7 +1,7 @@
 // FIXME: Update this file to be type safe and remove this and next line
 // @ts-strict-ignore
 import { CommonModule } from "@angular/common";
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 // This import has been flagged as unallowed for this class. It may be involved in a circular dependency loop.
 // eslint-disable-next-line no-restricted-imports
@@ -12,7 +12,12 @@ import { ClientType } from "@bitwarden/common/enums";
 import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
 import { CipherView } from "@bitwarden/common/vault/models/view/cipher.view";
 import { FolderView } from "@bitwarden/common/vault/models/view/folder.view";
-import { CardComponent, FormFieldModule, TypographyModule } from "@bitwarden/components";
+import {
+  ButtonLinkDirective,
+  CardComponent,
+  FormFieldModule,
+  TypographyModule,
+} from "@bitwarden/components";
 
 import { OrgIconDirective } from "../../components/org-icon.directive";
 
@@ -26,21 +31,35 @@ import { OrgIconDirective } from "../../components/org-icon.directive";
     TypographyModule,
     OrgIconDirective,
     FormFieldModule,
+    ButtonLinkDirective,
   ],
 })
-export class ItemDetailsV2Component {
+export class ItemDetailsV2Component implements OnInit {
   @Input() cipher: CipherView;
   @Input() organization?: Organization;
   @Input() collections?: CollectionView[];
   @Input() folder?: FolderView;
   @Input() hideOwner?: boolean = false;
 
-  clientType: ClientType = this.platformUtilsService.getClientType();
+  hasMultipleCollections: boolean = false;
+  showAllDetails: boolean = false;
+  // clientType: ClientType = platformUtilsService.getClientType();
+  private clientType: ClientType;
 
-  constructor(private platformUtilsService: PlatformUtilsService) {}
+  constructor(private platformUtilsService: PlatformUtilsService) {
+    this.clientType = this.platformUtilsService.getClientType();
+  }
+
+  ngOnInit() {
+    this.hasMultipleCollections = this.cipher?.collectionIds?.length > 1;
+  }
 
   get showOwnership() {
     return this.cipher.organizationId && this.organization && !this.hideOwner;
+  }
+
+  toggleShowMore() {
+    this.showAllDetails = !this.showAllDetails;
   }
 
   protected readonly ClientType = ClientType;
