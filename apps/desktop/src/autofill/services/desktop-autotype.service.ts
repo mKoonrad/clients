8e5b1df1bce8c14d20865705bafe38f13c12c55b
Colton Hurst <colton@coltonhurst.com>
2025-07-07 11:52:04 -0400

    [PM-22783] Add todo comments

--- apps/desktop/src/autofill/services/desktop-autotype.service.ts
+++ apps/desktop/src/autofill/services/desktop-autotype.service.ts
@@ -67,11 +67,13 @@ export class DesktopAutotypeService implements OnDestroy {
     });
   }
 
+  // TODO: this will call into desktop native code
   private enableAutotype() {
     // eslint-disable-next-line no-console
     console.log("Enabling Autotype...");
   }
 
+  // TODO: this will call into desktop native code
   private disableAutotype() {
     // eslint-disable-next-line no-console
     console.log("Disabling Autotype...");
