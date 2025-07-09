c23e3b88cb57493053247f4f75a1392c84ababa6
Bernd Schoolmann <mail@quexten.com>
2025-07-09 11:22:46 +0200

    Undo changes to feature flag service

--- libs/common/src/enums/feature-flag.enum.ts
+++ libs/common/src/enums/feature-flag.enum.ts
@@ -120,9 +120,9 @@ export const DefaultFeatureFlagValue = {
   /* Key Management */
   [FeatureFlag.PrivateKeyRegeneration]: FALSE,
   [FeatureFlag.PM4154_BulkEncryptionService]: FALSE,
-  [FeatureFlag.UseSDKForDecryption]: true,
+  [FeatureFlag.UseSDKForDecryption]: FALSE,
   [FeatureFlag.PM17987_BlockType0]: FALSE,
-  [FeatureFlag.EnrollAeadOnKeyRotation]: true,
+  [FeatureFlag.EnrollAeadOnKeyRotation]: FALSE,
 
   /* Platform */
   [FeatureFlag.IpcChannelFramework]: FALSE,
