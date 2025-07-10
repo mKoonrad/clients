f46f3ecf542fd318ca1fb1fd9b243025c169a145
jng <jng@bitwarden.com>
2025-07-10 11:11:51 -0400

    updates to move display logic to ts file accommodating screen size

--- libs/vault/src/cipher-view/item-details/item-details-v2.component.ts
+++ libs/vault/src/cipher-view/item-details/item-details-v2.component.ts
@@ -1,14 +1,14 @@
 // FIXME: Update this file to be type safe and remove this and next line
 // @ts-strict-ignore
-import { CommonModule } from "@angular/common";
-import { Component, Input, OnInit } from "@angular/core";
+import { CommonModule, DOCUMENT } from "@angular/common";
+import { Component, inject, input, OnChanges } from "@angular/core";
 
 // This import has been flagged as unallowed for this class. It may be involved in a circular dependency loop.
 // eslint-disable-next-line no-restricted-imports
 import { CollectionView } from "@bitwarden/admin-console/common";
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { Organization } from "@bitwarden/common/admin-console/models/domain/organization";
-import { ClientType } from "@bitwarden/common/enums";
+import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { CipherView } from "@bitwarden/common/vault/models/view/cipher.view";
 import { FolderView } from "@bitwarden/common/vault/models/view/folder.view";
 import {
@@ -33,27 +33,99 @@ import { OrgIconDirective } from "../../components/org-icon.directive";
     ButtonLinkDirective,
   ],
 })
-export class ItemDetailsV2Component implements OnInit {
-  @Input() cipher: CipherView;
-  @Input() organization?: Organization;
-  @Input() collections?: CollectionView[];
-  @Input() folder?: FolderView;
-  @Input() hideOwner?: boolean = false;
+export class ItemDetailsV2Component implements OnChanges {
+  hideOwner = input<boolean>(false);
+  cipher = input.required<CipherView>();
+  organization = input<Organization | undefined>();
+  folder = input<FolderView | undefined>();
+  collections = input<CollectionView[] | undefined>();
 
   hasMultipleCollections: boolean = false;
   showAllDetails: boolean = false;
 
-  ngOnInit() {
-    this.hasMultipleCollections = this.cipher?.collectionIds?.length > 1;
+  // Array to hold all details of item. Organization, Collections, and Folder
+  allItems: any[] = [];
+
+  // Array to hold the display pieces of the item details (dependent on length of array and screen size)
+  showItems: any[] = [];
+
+  // Inject the document to check for screen size
+  private document = inject(DOCUMENT);
+  isSmallScreen = false;
+
+  constructor(private i18nService: I18nService) {}
+
+  ngOnChanges() {
+    this.allItems = [];
+    this.hasMultipleCollections = this.cipher().collectionIds?.length > 1;
+    this.isSmallScreen = this.hasSmallScreen();
+
+    if (this.showOwnership && this.organization()) {
+      this.allItems.push(this.organization());
+    }
+    if (this.cipher().collectionIds?.length > 0 && this.collections()) {
+      this.allItems = [...this.allItems, ...this.collections()];
+    }
+    if (this.cipher().folderId && this.folder()) {
+      this.allItems.push(this.folder());
+    }
+    this.showItems = [...this.allItems];
+    if (this.isSmallScreen && this.allItems.length > 2) {
+      this.setItemsForSmallScreen();
+    }
   }
 
   get showOwnership() {
-    return this.cipher.organizationId && this.organization && !this.hideOwner;
+    return this.cipher().organizationId && this.organization() && !this.hideOwner();
   }
 
   toggleShowMore() {
     this.showAllDetails = !this.showAllDetails;
+    if (this.showAllDetails) {
+      this.showItems = [...this.allItems];
+    } else {
+      this.showItems = this.allItems.slice(0, 2);
+    }
   }
 
-  protected readonly ClientType = ClientType;
+  hasSmallScreen() {
+    return this.document.documentElement.clientWidth < 640;
+  }
+
+  setItemsForSmallScreen() {
+    this.showItems = this.allItems.slice(0, 2);
+  }
+
+  getAriaLabel(item: Organization | CollectionView | FolderView): string {
+    if (item instanceof Organization) {
+      return this.i18nService.t("owner") + item.name;
+    } else if (item instanceof CollectionView) {
+      return this.i18nService.t("collection") + item.name;
+    } else if (item instanceof FolderView) {
+      return this.i18nService.t("folder") + item.name;
+    }
+    return "";
+  }
+
+  getIconClass(item: CollectionView | FolderView): string {
+    if (item instanceof CollectionView) {
+      return "bwi-collection-shared";
+    } else if (item instanceof FolderView) {
+      return "bwi-folder";
+    }
+    return "";
+  }
+
+  getItemTitle(item: CollectionView | FolderView): string {
+    if (item instanceof CollectionView) {
+      return this.i18nService.t("collection");
+    } else if (item instanceof FolderView) {
+      return this.i18nService.t("folder");
+    }
+    return "";
+  }
+
+  isOrgIcon(item: Organization | CollectionView | FolderView): boolean {
+    return item instanceof Organization;
+  }
 }
