fa318fed96198f4335bf9d475a7e61b5f55247af
jaasen-livefront <jaasen@livefront.com>
2025-07-29 15:46:15 -0700

    don't enable sshkey form

--- libs/vault/src/cipher-form/components/item-details/item-details-section.component.ts
+++ libs/vault/src/cipher-form/components/item-details/item-details-section.component.ts
@@ -20,6 +20,7 @@ import { ConfigService } from "@bitwarden/common/platform/abstractions/config/co
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { Utils } from "@bitwarden/common/platform/misc/utils";
 import { CollectionId, OrganizationId, UserId } from "@bitwarden/common/types/guid";
+import { CipherType } from "@bitwarden/common/vault/enums";
 import { CipherView } from "@bitwarden/common/vault/models/view/cipher.view";
 import {
   CardComponent,
@@ -248,7 +249,7 @@ export class ItemDetailsSectionComponent implements OnInit {
       if (this.itemDetailsForm.controls.organizationId.value === null) {
         this.cipherFormContainer.disableFormFields();
         this.itemDetailsForm.controls.organizationId.enable();
-      } else {
+      } else if (this.originalCipherView?.type !== CipherType.SshKey) {
         this.cipherFormContainer.enableFormFields();
       }
     }
